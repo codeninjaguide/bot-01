354b2b2e44f5ce8d51937b65b8899613a5ccf623
DataWithVishal <webinspiration18@gmail.com>
2021-03-28 21:45:42 +0530

    masti sujal

--- bot.py
+++ bot.py
@@ -97,7 +97,7 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("pttest", help))
+    dp.add_handler(CommandHandler("pttest", pttest))
 
     # log all errors
     dp.add_error_handler(error)
